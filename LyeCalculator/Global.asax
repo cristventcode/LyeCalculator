6b8e7cc99f40bbd84adfb14d31411b85bee12e89
Cristian Ventura <cristian101ventura@yahoo.com>
2017-04-13 16:17:52 -0400

    Adding the first files.

---
+++ LyeCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LyeCalculator.MvcApplication" Language="C#" %>
---
+++ LyeCalculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LyeCalculator.Startup))]
+namespace LyeCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
