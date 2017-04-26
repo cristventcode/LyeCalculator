e39127799b48fff7123c7e5a75229fa9b864dcca
Cristian Ventura <cristian101ventura@yahoo.com>
2017-04-26 17:45:43 -0400

    Added ability to find details, based on ingredient selection

--- LyeCalculator/Scripts/App/app.js
+++ LyeCalculator/Scripts/App/app.js
@@ -6,27 +6,38 @@ var Main;
         return ingredient;
     }());
     ;
-    var session = (function () {
-        function session() {
+    var sessionDetails = (function () {
+        function sessionDetails() {
+            this.ingredients = [];
         }
-        return session;
+        return sessionDetails;
     }());
     ;
     var sampleData = [
-        { name: "Almond Oil", solid: .14, liquid: .21 },
-        { name: "Avocado Butter", solid: .13, liquid: .20 },
-        { name: "Castor Oil", solid: .14, liquid: .19 },
-        { name: "Kokum Butter", solid: .14, liquid: .20 },
-        { name: "Mango Butter", solid: .14, liquid: .20 },
+        { id: 1, name: "Almond Oil", solid: .14, liquid: .21 },
+        { id: 2, name: "Avocado Butter", solid: .13, liquid: .20 },
+        { id: 3, name: "Castor Oil", solid: .14, liquid: .19 },
+        { id: 4, name: "Kokum Butter", solid: .14, liquid: .20 },
+        { id: 5, name: "Mango Butter", solid: .14, liquid: .20 },
     ];
+    var thisSession = new sessionDetails;
+    var tolistBtn = document.getElementById("tolist-btn"), listHolder, amountTableItem = document.getElementById("amount-table-items");
     (function loadSelectList() {
-        var listHolder = document.getElementById("ingredient-select");
+        listHolder = document.getElementById("ingredient-select");
         for (var ing in sampleData) {
             var element = document.createElement("option");
             element.innerText = sampleData[ing].name;
+            element.value = sampleData[ing].id.toString();
             listHolder.appendChild(element);
         }
         ;
     })();
+    tolistBtn.addEventListener("click", function () {
+        var selectionValue = listHolder.value;
+        thisSession.ingredients.push(1);
+        var ingDetials = sampleData.filter(function (item) { return item.id == selectionValue; })[0];
+        alert(ingDetials.name);
+        var ingTemplate = "<td><span class=\"ingredient-item\"></span></td >\n                           <td><input class=\"form-control\" />0</td>\n                           <td class=\"text-center\"> <button class=\"btn btn-xs btn-default delete-this-btn\" > X </button></td>";
+    });
 })(Main || (Main = {}));
 //# sourceMappingURL=app.js.map
--- LyeCalculator/src/app.ts
+++ LyeCalculator/src/app.ts
@@ -1,34 +1,55 @@
 ﻿
 namespace Main {
+
     class ingredient {
+        id: number
         name: string;
         solid: number;
         liquid: number;
     };
 
-    class session {
+    class sessionDetails {
         type: string;
         unit: string;
         superfat: number;
+        ingredients: number[] = [];
     };
 
     const sampleData: ingredient[] = [
-        { name: "Almond Oil", solid: .14, liquid: .21 },
-        { name: "Avocado Butter", solid: .13, liquid: .20 },
-        { name: "Castor Oil", solid: .14, liquid: .19 },
-        { name: "Kokum Butter", solid: .14, liquid: .20 },
-        { name: "Mango Butter", solid: .14, liquid: .20 },
+        { id: 1, name: "Almond Oil", solid: .14, liquid: .21 },
+        { id: 2, name: "Avocado Butter", solid: .13, liquid: .20 },
+        { id: 3, name: "Castor Oil", solid: .14, liquid: .19 },
+        { id: 4, name: "Kokum Butter", solid: .14, liquid: .20 },
+        { id: 5, name: "Mango Butter", solid: .14, liquid: .20 },
     ];
 
+    let thisSession = new sessionDetails;
+
+    let tolistBtn = document.getElementById("tolist-btn"),
+        listHolder: any,
+        amountTableItem = document.getElementById("amount-table-items");
+
     (function loadSelectList() {
-        let listHolder = document.getElementById("ingredient-select");
+        listHolder = document.getElementById("ingredient-select");
 
         for (let ing in sampleData) {
             let element = document.createElement("option");
             element.innerText = sampleData[ing].name;
+            element.value = sampleData[ing].id.toString();
             listHolder.appendChild(element);
         };
-
     })();
+
+    tolistBtn.addEventListener("click", function () {
+        let selectionValue = listHolder.value;
+        thisSession.ingredients.push(1);
+        
+        let ingDetials = sampleData.filter(item => item.id == selectionValue)[0];
+        alert(ingDetials.name);
+
+        let ingTemplate = `<td><span class="ingredient-item"></span></td >
+                           <td><input class="form-control" />0</td>
+                           <td class="text-center"> <button class="btn btn-xs btn-default delete-this-btn" > X </button></td>`;
+    });
 }
 
