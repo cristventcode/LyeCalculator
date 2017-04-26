705c5bf7d3a1b79a8edb34f43dfd7464bd358b58
Cristian Ventura <cristian101ventura@yahoo.com>
2017-04-26 10:39:51 -0400

    Started adding code to ts file

--- LyeCalculator/Scripts/App/app.js
+++ LyeCalculator/Scripts/App/app.js
@@ -1,6 +1,32 @@
-function sayHello() {
-    var compiler = document.getElementById("compiler").value;
-    var framework = document.getElementById("framework").value;
-    return "Hello from " + compiler + " and " + framework + "!";
-}
+var Main;
+(function (Main) {
+    var ingredient = (function () {
+        function ingredient() {
+        }
+        return ingredient;
+    }());
+    ;
+    var session = (function () {
+        function session() {
+        }
+        return session;
+    }());
+    ;
+    var sampleData = [
+        { name: "Almond Oil", solid: .14, liquid: .21 },
+        { name: "Avocado Butter", solid: .13, liquid: .20 },
+        { name: "Castor Oil", solid: .14, liquid: .19 },
+        { name: "Kokum Butter", solid: .14, liquid: .20 },
+        { name: "Mango Butter", solid: .14, liquid: .20 },
+    ];
+    (function loadSelectList() {
+        var listHolder = document.getElementById("ingredient-select");
+        for (var ing in sampleData) {
+            var element = document.createElement("option");
+            element.innerText = sampleData[ing].name;
+            listHolder.appendChild(element);
+        }
+        ;
+    })();
+})(Main || (Main = {}));
 //# sourceMappingURL=app.js.map
--- LyeCalculator/src/app.ts
+++ LyeCalculator/src/app.ts
@@ -1,5 +1,34 @@
-﻿function sayHello() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Hello from ${compiler} and ${framework}!`;
+﻿
+namespace Main {
+    class ingredient {
+        name: string;
+        solid: number;
+        liquid: number;
+    };
+
+    class session {
+        type: string;
+        unit: string;
+        superfat: number;
+    };
+
+    const sampleData: ingredient[] = [
+        { name: "Almond Oil", solid: .14, liquid: .21 },
+        { name: "Avocado Butter", solid: .13, liquid: .20 },
+        { name: "Castor Oil", solid: .14, liquid: .19 },
+        { name: "Kokum Butter", solid: .14, liquid: .20 },
+        { name: "Mango Butter", solid: .14, liquid: .20 },
+    ];
+
+    (function loadSelectList() {
+        let listHolder = document.getElementById("ingredient-select");
+
+        for (let ing in sampleData) {
+            let element = document.createElement("option");
+            element.innerText = sampleData[ing].name;
+            listHolder.appendChild(element);
+        };
+
+    })();
 }
+
